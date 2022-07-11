247ec638c36317719c7ee61f494f5e12276421e6
Dulovania <ryhank9@gmail.com>
2022-07-12 06:06:55 +1200

    Draaw rectangle and added color

--- OpenGL/Scripts/Source.cpp
+++ OpenGL/Scripts/Source.cpp
@@ -1,4 +1,9 @@
+#include <glew.h>
 #include <glfw3.h>
+#include <iostream>
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 600
 
 int main(void)
 {
@@ -7,9 +12,10 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
+    
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Ranut has small bols", NULL, NULL);
+
     if (!window)
     {
         glfwTerminate();
@@ -19,6 +25,27 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK) std::cout << "Error: glewInit non-operational";
+
+    float positions[] = 
+    { 
+        0.5f, -0.5f,
+        -0.5f, -0.5f,
+        -0.5f, 0.5f,
+        0.5f, 0.5f,
+    };
+
+    unsigned int buffer;
+
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -26,11 +53,8 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Create triangle.
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glColor3f(1.0f, 0.2f, 0.2f); //sets color
+        glDrawArrays(GL_QUADS, 0, 4);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
