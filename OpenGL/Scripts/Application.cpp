cb039633ae59ebb1e9f148a4435fc5738791111d
Dulovania <ryhank9@gmail.com>
2023-06-18 03:29:23 +1200

    Added debugging

--- OpenGL/Scripts/Application.cpp
+++ OpenGL/Scripts/Application.cpp
@@ -3,6 +3,28 @@
 #include <fstream>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+// Use ASSERT(GLLogCall()) to debug.
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 // Struct contains Vertex and Fragment data.
 struct ShaderProgramSource
 {
