d3734c10503901c0d1ffa47a66fa7ecdbe2c45af
Dulovania <ryhank9@gmail.com>
2024-08-14 19:39:25 +1000

    Added engine library header files

---
+++ Orbiter/Scripts/Headers/Core/orbitercore.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Core/input.h"
+#include "Core/animationplayer.h"
+#include "Core/animationframe.h"
+#include "Core/filemanager.h"
+#include "Core/renderer.h"
+#include "Core/gameobject.h"
